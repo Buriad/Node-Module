deb3bc87d1d2f1fbe27c7e664f3ef581383f6efd
mstars_lab2_14 <mstars_lab2_14@mstars-lab2-14s-iMac.local>
2022-05-19 14:53:22 +0800

    to push DB-07-Ex

---
+++ DB-07-Ex/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Tuya@1978",
+    database: 'employees',
+    multipleStatements: true,
+});
+
+connection.connect((err) => {
+    if (!err) {
+        console.log("Database connected successfully");
+    } else {
+        console.log("error")
+    }
+});
+
+app.get('/company', (req, res) => {
+    const request = req.query;
+    console.log(request);
+
+    connection.query(
+        `select count(*) as positionNumbers from employees where emp_no IN (select emp_no from titles where title = ?)`,
+        [request.title],
+        (err, rows, fields) => {
+            if (!err) {
+                res.send(rows);
+            } else {
+                console.log(err);
+                res.send(err);
+            }
+        }
+    );
+});
+
+
+
+app.get('/managers/salary', (req, res) => {
+    const request = req.query;
+    console.log(request);
+
+    connection.query(
+        `select * from dept_manager d left join salaries s on d.emp_no = s.emp_no;`,
+        [request.salaries],
+        (err, rows, fields) => {
+            if (!err) {
+                res.send(rows);
+            } else {
+                console.log(err);
+                res.send(err);
+            }
+        }
+    );
+});
+
+
+app.listen(3000, () => {
+
+}); 
